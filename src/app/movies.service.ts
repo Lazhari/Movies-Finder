01c614495ef5226e41c63eb4c9bbb1a5cd92b60f
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2016-11-16 20:56:15 +0000

    Movies search service

---
+++ src/app/movies.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {Jsonp, URLSearchParams} from '@angular/http';
+import 'rxjs/Rx';
+
+@Injectable()
+export class MoviesService {
+  apikey: string;
+
+  constructor(private _jsonp: Jsonp) {
+    this.apikey = 'fed69657ba4cc6e1078d2a6a95f51c8c';
+    console.log('Movies service is ready');
+  }
+
+  getPopular() {
+    var search = new URLSearchParams();
+    search.set('sort_by','popularity.desc');
+    search.set('api_key', this.apikey);
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK', {search})
+      .map(res => {
+        return res.json();
+      })
+  }
+
+  getInTheaters() {
+    var search = new URLSearchParams();
+    search.set('primary_release_date.gt', '2015-10-20');
+    search.set('primary_release_date.lte', '2015-12-20');
+    search.set('sort_by','popularity.desc');
+    search.set('api_key', this.apikey);
+    return this._jsonp.get('https://api.themoviedb.org/3/discover/movie?callback=JSONP_CALLBACK', {search})
+      .map(res => {
+        return res.json();
+      })
+  }
+
+}
