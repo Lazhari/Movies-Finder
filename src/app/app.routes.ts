1e39112068f77b0dc28ddb47eda507dc23a5b52c
Lazhari Mohammed <lazhari.mohammed@outlook.com>
2016-11-18 02:58:11 +0000

    update application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MoviesComponent } from './movies/movies.component';
 import { MovieComponent } from './movie/movie.component';
+import { GenresComponent } from './genres/genres.component';
 
 export const appRoutes: Routes = [
     {path: '', component: MoviesComponent},
-    {path: 'movie/:id', component: MovieComponent}
+    {path: 'movie/:id', component: MovieComponent},
+    {path: 'genres/:id/:name', component: GenresComponent},
 ];
